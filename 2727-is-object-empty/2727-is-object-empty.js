b2cc31ad7031cbaccf23c41f6d0140e89683bcfb
Harikant Bajaj <114638679+harikantbajaj@users.noreply.github.com>
2024-04-01 03:01:46 +0530

    Time: 56 ms (31.62%), Space: 49.2 MB (71.10%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,23 @@
+function isEmpty(data) {
+  if (Array.isArray(data)) {
+    return data.length === 0;  
+  } else if (typeof data === 'object') {
+    return Object.keys(data).length === 0;  
+  } else {
+    
+      
+      
+      return false;
+
+  
+  
+  
+  
+  
+  
+  
+  }
+
+
+
+}
